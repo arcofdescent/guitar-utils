f88ff60467bfa9884c1d419daccd3ac9b2b74ad0
Rohan Almeida <almeida@aarohan.biz>
2016-11-16 18:16:00 +0530

    get_pitch_number added

--- README.md
+++ README.md
@@ -10,10 +10,12 @@ Utilities for guitar fretboard and audio
 var gUtils = require('guitar-utils');
 
 var p = gUtils.get_pitch_at(5, 0); // p will be 'A'
+var pn = gUtils.get_pitch_number(5, 0); // pn will be 2
 
 ```
-**get_pitch_at()** takes two arguments. First is the string (1-6),  second is the
-fret (0-24). Fret 0 means open string :)
+**get_pitch_at** and **get_pitch_number** take two arguments. 
+First is the string (1-6),  second is the fret (0-24). 
+Fret 0 means open string :)
 
 ### Installation
 
--- src/index.js
+++ src/index.js
@@ -28,5 +28,55 @@ module.exports = {
     return p;
   },
 
+  get_pitch_number: function(string = "1", fret="3") {
+
+    let pitch_number;
+
+    if (
+      (string == 6 && (fret >= 0 && fret <= 7)) ||
+      (string == 5 && (fret >= 0 && fret <= 2))
+    )
+    {
+      pitch_number = 2;
+    }
+    else if (
+      (string == 6 && (fret >= 8 && fret <= 19)) ||
+      (string == 5 && (fret >= 3 && fret <= 14)) ||
+      (string == 4 && (fret >= 0 && fret <= 9)) ||
+      (string == 3 && (fret >= 0 && fret <= 4)) ||
+      (string == 2 && fret == 0)
+    )
+    {
+      pitch_number = 3;
+    }
+    else if (
+      (string == 6 && (fret >= 20 && fret <= 24)) ||
+      (string == 5 && (fret >= 15 && fret <= 24)) ||
+      (string == 4 && (fret >= 10 && fret <= 21)) ||
+      (string == 3 && (fret >= 5 && fret <= 16)) ||
+      (string == 2 && (fret >= 1 && fret <= 12)) ||
+      (string == 1 && (fret >= 0 && fret <= 7))
+    )
+    {
+      pitch_number = 4;
+    }
+    else if (
+      (string == 4 && (fret >= 22 && fret <= 24)) ||
+      (string == 3 && (fret >= 17 && fret <= 24)) ||
+      (string == 2 && (fret >= 13 && fret <= 24)) ||
+      (string == 1 && (fret >= 8 && fret <= 19))
+    )
+    {
+      pitch_number = 5;
+    }
+    else if (
+      (string == 1 && fret >= 20)
+    )
+    {
+      pitch_number = 6;
+    }
+
+    return pitch_number;
+  },
 };
 
